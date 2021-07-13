6b72785a470dcd17275793c7b7b015c52711a84f
Matheus Coutinho <mcsl.hef@uea.edu.br>
2021-07-13 14:58:57 -0400

    teste 2

--- nlw06/src/server.js
+++ nlw06/src/server.js
@@ -1,11 +1,3 @@
-const express = require('express');
-//  const route =  require('./route');
-
-const server = express();
-
-
-// server.set('view engine', 'ejs')
-
-// server.use(route)
-
-server.listen(3000, () => console.log("ON FIRE")) 
+aaaaaa
+bbbbbbbb
+cccccc
