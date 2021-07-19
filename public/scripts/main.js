e9a0b6574f1fbdf46e451fdb6149e613eae41148
Matheus Coutinho <mcsl.hef@uea.edu.br>
2021-07-19 10:22:25 -0400

    atualição button

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,6 +3,12 @@ import Modal from "./modal.js"
 
 const modal = Modal()
 
+const modalTitle = document.querySelector(".modal h2")
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+
+
 
 // Pegar todos os botões que existe com a classe check 
 const checkButtons = document.querySelectorAll(".actions a.check")
@@ -11,14 +17,28 @@ const checkButtons = document.querySelectorAll(".actions a.check")
 checkButtons.forEach(button => {
     // Adicionar a escuta
     button.addEventListener("click", event => {
+
+
+        modalTitle.innerHTML = "Marcar como lida"
+
+
         // Abril modal
         modal.open()
     })
 
 })
 
+// Quando o botão delete for clicado ele abre a modal
+
+const deleteButton = document.querySelectorAll(".actions a.delete")
+
+deleteButton.forEach(button => {
+    button.addEventListener("click", event => {
+
+        modalTitle.innerHTML = "Excluir essa pergunta"
 
 
-
-// Pegar quando o marcar como lido for clicado
+        modal.open()
+    })
+})
 
--- src/route.js
+++ src/route.js
@@ -4,7 +4,7 @@ const route = express.Router();
 
 route.get('/', (req, res) => res.render("index"));
 route.get('/room',(req,res) => res.render("room"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/create-pass',(req, res) => res.render("create-pass"))
 
 
 module.exports = route  
