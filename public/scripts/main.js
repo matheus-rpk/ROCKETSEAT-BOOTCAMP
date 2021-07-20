f276211ac62af325e7abda748ebb6d90d1c1e5f7
Matheus Coutinho <mcsl.hef@uea.edu.br>
2021-07-20 16:20:59 -0400

    wrapper

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,25 +7,13 @@ const modalTitle = document.querySelector(".modal h2")
 const modalDescription = document.querySelector('.modal p')
 const modalButton = document.querySelector('.modal button')
 
-
-
-
 // Pegar todos os botões que existe com a classe check 
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 
 checkButtons.forEach(button => {
     // Adicionar a escuta
-    button.addEventListener("click", event => {
-
-
-        modalTitle.innerHTML = "Marcar como lida"
-
-
-        // Abril modal
-        modal.open()
-    })
-
+    button.addEventListener("click",handleClick)
 })
 
 // Quando o botão delete for clicado ele abre a modal
@@ -33,12 +21,21 @@ checkButtons.forEach(button => {
 const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button => {
-    button.addEventListener("click", event => {
-
-        modalTitle.innerHTML = "Excluir essa pergunta"
-
-
-        modal.open()
-    })
+    button.addEventListener("click", (event) => handleClick(event, false)) 
 })
+
+    function handleClick(event, check = true) {
+        event.preventDefault()
+        const text =  check ? "Marcar como lida" : "Excluir"
+
+        modalTitle.innerHTML = `${text} esta pergunta`
+        modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?` 
+        modalButton.innerHTML = `sim, ${text.toLowerCase()}`
+        check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+        
+        modal.open()
+}
+
+
+    
 
